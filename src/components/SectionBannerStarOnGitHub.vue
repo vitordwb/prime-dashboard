35d229ef03da307bb1818916d38bdb8c297165e1
Vitor <64985648+vitordwb@users.noreply.github.com>
2024-05-13 21:10:29 -0300

    updating SectionBannerStarOnGitHub

--- src/components/SectionBannerStarOnGitHub.vue
+++ src/components/SectionBannerStarOnGitHub.vue
@@ -2,12 +2,12 @@
 import { mdiGithub } from '@mdi/js'
 import BaseButton from '@/components/BaseButton.vue'
 import SectionBanner from '@/components/SectionBanner.vue'
-import { gradientBgPinkRed } from '@/colors'
+import { gradientBgDark } from '@/colors'
 </script>
 
 <template>
-  <SectionBanner :class="gradientBgPinkRed">
-    <h1 class="text-3xl text-white mb-6">Like the project? Please star on <b>GitHub</b> ;-)</h1>
+  <SectionBanner :class="gradientBgDark">
+    <h1 class="text-3xl text-white mb-6">Código disponível no <b>GitHub</b> :)</h1>
     <div>
       <BaseButton
         href="https://github.com/vitordwb/prime-dashboard"
