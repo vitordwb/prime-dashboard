766d306e3147551e501b64e6ff69f363af303c35
Vitor <64985648+vitordwb@users.noreply.github.com>
2024-05-17 10:21:43 -0300

    adding BarChart component

---
+++ src/components/Charts/BarChart.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref, watch, computed, onMounted } from 'vue'
+import {
+  Chart,
+  PointElement,
+  LinearScale,
+  CategoryScale,
+  Tooltip,
+  BarElement,
+  BarController
+} from 'chart.js'
+
+const props = defineProps({
+  data: {
+    type: Object,
+    required: true
+  }
+})
+
+const root = ref(null)
+
+let chart
+
+Chart.register(BarElement, PointElement, BarController, LinearScale, CategoryScale, Tooltip)
+
+onMounted(() => {
+  chart = new Chart(root.value, {
+    type: 'bar',
+    data: props.data,
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        y: {
+          display: false
+        },
+        x: {
+          display: true
+        }
+      },
+      plugins: {
+        legend: {
+          display: false
+        }
+      }
+    }
+  })
+})
+
+const chartData = computed(() => props.data)
+
+watch(chartData, (data) => {
+  if (chart) {
+    chart.data = data
+    chart.update()
+  }
+})
+</script>
+
+<template>
+  <canvas ref="root" />
+</template>
