79900a095ce6848cbb724aa41c5409fb61de6abc
Vitor <64985648+vitordwb@users.noreply.github.com>
2024-05-17 10:20:17 -0300

    updating ChartsView

--- src/views/ChartsView.vue
+++ src/views/ChartsView.vue
@@ -3,33 +3,88 @@ import SectionTitle from '@/components/SectionTitle.vue'
 import SectionMain from '@/components/SectionMain.vue'
 import LayoutAuthenticated from '@/layouts/LayoutAuthenticated.vue'
 
-import { Bar } from 'vue-chartjs'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+import { Bar, Line } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, LineElement } from 'chart.js'
+import { useFirebaseStore } from '@/stores/firebaseStore'
+import { computed, onMounted, ref } from 'vue'
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(Title, Tooltip, Legend, BarElement, LineElement, CategoryScale, LinearScale)
 
 export default {
   name: 'BarChart',
-  components: { LayoutAuthenticated, SectionMain, SectionTitle, Bar },
+  // eslint-disable-next-line vue/no-reserved-component-names
+  components: { LayoutAuthenticated, SectionMain, SectionTitle, Bar, Line },
+  setup() {
+    const firebaseStore = useFirebaseStore();
+
+    const relayData = computed(() => firebaseStore.relayData);
+    const loading = computed(() => firebaseStore.loading);
+    const error = computed(() => firebaseStore.error);
+
+    const getChartData = (data, info) => {
+      return data.map((value) => {
+        const chartData = value[info];
+        if (!(chartData[0])) {
+          return [value['datetime'], 0];
+        } else {
+          return [value['datetime'], chartData[1]];
+        }
+      });
+    };
+
+    const updateChartData = () => {
+      if (relayData.value.length > 0) {
+        const data = getChartData(relayData.value, 'temperatura_interna');
+        this.chartData.value.labels = data.map(x => x[0]);
+        this.chartData.value.datasets[0].data = data.map(x => x[1]);
+      }
+    };
+
+    onMounted(() => {
+      firebaseStore.fetchRelayData();
+      updateChartData();
+
+      // Atualizar os dados do gráfico a cada 60 segundos
+      setInterval(() => {
+        firebaseStore.fetchRelayData();
+        updateChartData();
+      }, 10000);
+    });
+
+    return {
+      relayData,
+      loading,
+      error,
+      getChartData
+    };
+  },
+
   data() {
     return {
-      chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      },
+      chartData: ref({
+        labels: this.getChartData(this.relayData, 'temperatura_interna').map(x => x[0]),
+        datasets: [ { data: this.getChartData(this.relayData, 'temperatura_interna').map(x => x[1])}]
+      }),
       chartOptions: {
         responsive: true
       }
     }
   }
 }
-
-
 </script>
 
 <template>
   <LayoutAuthenticated>
-    <SectionTitle first>Gráficos | Monitoramento</SectionTitle>
+    <SectionTitle first>Gráficos | Temperatura Interna </SectionTitle>
+
+    <!--    Bar       -->
+    <!--    Doughnut  -->
+    <!--    Line      -->
+    <!--    Pie       -->
+    <!--    PolarArea -->
+    <!--    Radar     -->
+    <!--    Bubble    -->
+    <!--    Scatter   -->
 
     <SectionMain>
       <Bar
@@ -39,17 +94,25 @@ export default {
       />
     </SectionMain>
 
-    <SectionTitle>Small laptop 1024px</SectionTitle>
-
     <SectionMain>
-      <div
-        class="md:w-10/12 shadow-2xl md:mx-auto rounded-3xl border-8 border-white overflow-hidden"
-      >
-        <img
-          src="https://static.justboil.me/templates/one/one-tailwind-vue-1024.png"
-          class="block"
-        />
-      </div>
+      <Line
+        id="my-chart-id-1"
+        :options="chartOptions"
+        :data="chartData"
+      />
     </SectionMain>
+
+<!--    <SectionTitle>Small laptop 1024px</SectionTitle>-->
+
+<!--    <SectionMain>-->
+<!--      <div-->
+<!--        class="md:w-10/12 shadow-2xl md:mx-auto rounded-3xl border-8 border-white overflow-hidden"-->
+<!--      >-->
+<!--        <img-->
+<!--          src="https://static.justboil.me/templates/one/one-tailwind-vue-1024.png"-->
+<!--          class="block"-->
+<!--        />-->
+<!--      </div>-->
+<!--    </SectionMain>-->
   </LayoutAuthenticated>
 </template>
