83ffb49e2b5981fbb6c17108cccb5c45da1b2c20
Vitor <64985648+vitordwb@users.noreply.github.com>
2024-05-28 20:36:23 -0300

    CiclosChart: updating

--- src/components/Charts/CiclosChart.vue
+++ src/components/Charts/CiclosChart.vue
@@ -1,7 +1,15 @@
 <script>
-import { computed, onMounted } from 'vue'
-import { useFirebaseStore }    from '@/stores/firebaseStore'
-import BarChart                from '@/components/Charts/BarChart.vue'
+import { computed, onMounted }    from 'vue'
+import { useFirebaseStore }       from '@/stores/firebaseStore'
+import BarChart                   from '@/components/Charts/BarChart.vue'
+import BaseButton                 from '@/components/BaseButton.vue'
+import SectionTitleLineWithButton from '@/components/SectionTitleLineWithButton.vue'
+import CardBox                    from '@/components/CardBox.vue'
+
+import {
+  mdiReload,
+  mdiChartPie,
+} from '@mdi/js'
 
 import { Chart as ChartJS
   , Title
@@ -25,10 +33,9 @@ ChartJS.register(
 
 export default {
   name: 'CiclosChart',
-  components: { BarChart },
+  components: { CardBox, SectionTitleLineWithButton, BaseButton, BarChart },
   setup() {
     const firebaseStore = useFirebaseStore();
-
     const processedData = computed(() => firebaseStore.getProcessedData());
 
     onMounted(() => {
@@ -42,6 +49,8 @@ export default {
 
   data() {
     return {
+      mdiReload,
+      mdiChartPie,
       chartData: {
         "labels": this.processedData.getDatetime(),
         "datasets": [
@@ -56,10 +65,24 @@ export default {
         ]
       }
     }
+  },
+
+  methods: {
+    getAlert() {
+      alert('funcionou')
+    }
   }
 }
 </script>
 
 <template>
-  <bar-chart :data="chartData" class="h-96" />
+  <SectionTitleLineWithButton :icon="mdiChartPie" title="Ciclos">
+    <BaseButton :icon="mdiReload" color="whiteDark" @click="getAlert" />
+  </SectionTitleLineWithButton>
+
+  <CardBox class="mb-6">
+    <div>
+      <bar-chart :data="chartData" class="h-96" />
+    </div>
+  </CardBox>
 </template>
