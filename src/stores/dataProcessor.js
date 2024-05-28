5c0a9e36c6524c720781c6b650d8924cda8e53bc
Vitor <64985648+vitordwb@users.noreply.github.com>
2024-05-28 20:38:17 -0300

    dataProcessor: updating

--- src/stores/dataProcessor.js
+++ src/stores/dataProcessor.js
@@ -1,37 +1,29 @@
 export function processRelayData(data) {
   return {
-    getTemperaturaInterna: () => data.map(item => item.temperatura_interna[1]),
-    getCycle: () => data.map(item => item.cycle),
-    getDatetime: () => data.map(item => item.datetime),
-    getCorrenteBaixo: () => data.map(item => item.corrente_baixo[1]),
-    getCorrenteCima: () => data.map(item => item.corrente_cima[1]),
-    getRpmBaixo: () => data.map(item => item.rpm_baixo[1]),
-    getRpmCima: () => data.map(item => item.rpm_cima[1]),
-    getTemperaturaCobre: () => data.map(item => item.temperatura_cobre[1]),
-    getTemperaturaExterna: () => data.map(item => item.temperatura_externa[1]),
+    getCycle:                 () => data.map(item => item.cycle),
+    getDatetime:              () => data.map(item => item.datetime),
+
+    getTemperaturaCobre:      () => data.map(item => item.temperatura_cobre[1]),
+    getTemperaturaInterna:    () => data.map(item => item.temperatura_interna[1]),
+    getTemperaturaExterna:    () => data.map(item => item.temperatura_externa[1]),
+
+    getConsumoBaixo:          () => data.map(item => item.corrente_baixo[1].consumo),
+    getPotenciaAparenteBaixo: () => data.map(item => item.corrente_baixo[1].potencia_apa),
+    getPotenciaAtivaBaixo:    () => data.map(item => item.corrente_baixo[1].potencia_ativ),
+    getPotenciaRmsBaixo:      () => data.map(item => item.corrente_baixo[1].potencia_rms),
+
+    getConsumoCima:           () => data.map(item => item.corrente_cima[1].consumo),
+    getPotenciaAparenteCima:  () => data.map(item => item.corrente_cima[1].potencia_apa),
+    getPotenciaAtivaCima:     () => data.map(item => item.corrente_cima[1].potencia_ativ),
+    getPotenciaRmsCima:       () => data.map(item => item.corrente_cima[1].potencia_rms),
   };
 }
-
-// <!--    Bar       -->
-// <!--    Doughnut  -->
-// <!--    Line      -->
-// <!--    Pie       -->
-// <!--    PolarArea -->
-// <!--    Radar     -->
-// <!--    Bubble    -->
-// <!--    Scatter   -->
 
 // const chartData = {
 //   "labels": [
 //     "01",
 //     "02",
-//     "03",
-//     "04",
-//     "05",
-//     "06",
-//     "07",
-//     "08",
-//     "09"
+//     "03"
 //   ],
 //   "datasets": [
 //     {
