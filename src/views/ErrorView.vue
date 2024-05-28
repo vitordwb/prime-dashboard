d5900563261b26d5cac16f895f5bf332ee4e07d9
Vitor <64985648+vitordwb@users.noreply.github.com>
2024-05-28 20:40:45 -0300

    updating: ErrorView

--- src/views/ErrorView.vue
+++ src/views/ErrorView.vue
@@ -1,9 +1,9 @@
 <script setup>
 import SectionFullScreen from '@/components/SectionFullScreen.vue'
-import CardBox from '@/components/CardBox.vue'
-import BaseButton from '@/components/BaseButton.vue'
-import BaseButtons from '@/components/BaseButtons.vue'
-import LayoutGuest from '@/layouts/LayoutGuest.vue'
+import CardBox           from '@/components/CardBox.vue'
+import BaseButton        from '@/components/BaseButton.vue'
+import BaseButtons       from '@/components/BaseButtons.vue'
+import LayoutGuest       from '@/layouts/LayoutGuest.vue'
 </script>
 
 <template>
@@ -11,14 +11,14 @@ import LayoutGuest from '@/layouts/LayoutGuest.vue'
     <SectionFullScreen v-slot="{ cardClass }" bg="pinkRed">
       <CardBox :class="cardClass">
         <div class="space-y-3">
-          <h1 class="text-2xl">Unhandled exception</h1>
+          <h1 class="text-2xl">Aconteceu um erro</h1>
 
-          <p>An Error Occurred</p>
+          <p>Aconteceu um erro</p>
         </div>
 
         <template #footer>
           <BaseButtons>
-            <BaseButton label="Done" to="/dashboard" color="danger" />
+            <BaseButton label="Fechar" to="/dashboard" color="danger" />
           </BaseButtons>
         </template>
       </CardBox>
